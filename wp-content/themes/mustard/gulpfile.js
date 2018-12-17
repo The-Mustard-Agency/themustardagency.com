423943630975d2a7cf5459df731b9398cfbb6c9a
Mustard Mac <alexjhwoollam@gmail.com>
2018-12-17 16:33:52 +0000

    minified css

--- wp-content/themes/mustard/gulpfile.js
+++ wp-content/themes/mustard/gulpfile.js
@@ -53,7 +53,7 @@ gulp.task('imagemin', function () {
 gulp.task('sass', function () {
   gulp.src('./sass/*.scss')
     .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('.'));
